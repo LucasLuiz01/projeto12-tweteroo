f79a21069d9dc68232d21f57e58d13b7831a5896
Lucas Luiz <lucas.luiz10498@gmail.com>
2022-11-04 15:11:31 -0300

    versao sem os bonus

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import express from "express";
 const usuarios = [];
+const tweets = [];
 const app = express();
 app.use(express.json());
 
-app.post("/sign-up", (res, req) => {
+app.post("/sign-up", (req, res) => {
 usuarios.push(req.body);
-res.send("ok");
+res.status(201).send("ok login");
+return;
+})
+app.post("/tweets", (req, res) => {
+    tweets.push(req.body);
+    res.status(201).send("ok tweeter");
+    return;
+})
+
+app.get("/tweets", (req, res) => {
+  
+    res.send(tweets.slice(-10));
+    console.log(username, tweet);
 })
 
 app.listen(5000);
