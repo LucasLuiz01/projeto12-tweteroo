eea28a5dc3e342edec10ee63956384d1002ee68c
Lucas Luiz <lucas.luiz10498@gmail.com>
2022-11-05 13:00:57 -0300

    projeto pronto

--- index.js
+++ index.js
@@ -1,24 +1,57 @@
 import express from "express";
-const usuarios = [];
+import cors from "cors";
+const usuarios = {};
 const tweets = [];
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.post("/sign-up", (req, res) => {
-usuarios.push(req.body);
-res.status(201).send("ok login");
-return;
-})
-app.post("/tweets", (req, res) => {
-    tweets.push(req.body);
-    res.status(201).send("ok tweeter");
+  const { username, avatar } = req.body;
+  if (!username || !avatar) {
+    res.status(400).send("Todos os campos são obrigatórios!");
     return;
-})
+  }
+  usuarios[username] = avatar;
+  res.status(201).send("OK");
+  return;
+});
+app.post("/tweets", (req, res) => {
+  const { username, tweet } = req.body;
+  if (!username || !tweet) {
+    res.status(400).send("Todos os campos são obrigatórios!");
+    return;
+  }
+  tweets.push(req.body);
+  res.status(201).send(tweets);
+  return;
+});
 
 app.get("/tweets", (req, res) => {
-  
-    res.send(tweets.slice(-10));
-    console.log(username, tweet);
-})
+//   tweets = [
+//     { username: "bobesponja", tweet: "eu amo o hub" },
+//     { username: "bobesponja", tweet: "eu am o hub" },
+//     { username: "bobesponja", tweet: "eum o hub" },
+//   ];
+//   usuarios = {
+//     bobesponja:
+//       "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+//   };
+
+  const lastTweets = tweets.slice(-10);
+
+  let ret = [];
+  for (let tweet of lastTweets) {
+    const avatar = usuarios[tweet.username];
+
+    if (!avatar) {
+        continue;
+    }
+
+    ret = [...ret, { ...tweet, avatar: avatar }];
+  }
+
+  res.status(200).send(ret);
+});
 
 app.listen(5000);
